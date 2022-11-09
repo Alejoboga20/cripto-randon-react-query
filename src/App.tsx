6fd75f647bb964a054fe00f85509acdcc079542a
Alejo Boga <alejoboga20@gmail.com>
2022-11-09 15:55:52 -0500

    Create base case

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import './App.css';
 
 const getRandomNumberFromApi = async (): Promise<number> => {
@@ -12,16 +12,33 @@ const getRandomNumberFromApi = async (): Promise<number> => {
 
 export const App = () => {
 	const [number, setNumber] = useState<number>();
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState('');
+	const [key, forceRefetch] = useReducer((x: number) => x + 1, 0);
 
 	useEffect(() => {
-		getRandomNumberFromApi().then((numb) => setNumber(numb));
-	}, []);
+		setIsLoading(true);
+		getRandomNumberFromApi()
+			.then(setNumber)
+			.catch((error) => setError(error.message));
+	}, [key]);
+
+	useEffect(() => {
+		if (number) setIsLoading(false);
+	}, [number]);
+
+	useEffect(() => {
+		if (error) setIsLoading(false);
+	}, [error]);
 
 	return (
 		<div className='App'>
-			<h2>Random Number: {number}</h2>
+			{isLoading ? <h2>Loading...</h2> : <h2>Random Number: {number}</h2>}
+			{!isLoading && error && <h3>{error}</h3>}
+
+			<button onClick={forceRefetch} disabled={isLoading}>
+				{isLoading ? '...' : 'new number'}
+			</button>
 		</div>
 	);
 };
-
-//https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new
