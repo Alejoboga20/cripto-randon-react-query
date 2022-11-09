0f8c4d19ebe054737ea33a4bfcb9ce9e03842d82
Alejo Boga <alejoboga20@gmail.com>
2022-11-09 15:43:49 -0500

    fetch random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomNumberFromApi = async (): Promise<number> => {
+	const response = await fetch(
+		'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+	);
+	const numberString = await response.text();
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+	return +numberString;
+};
 
-export default App
+export const App = () => {
+	const [number, setNumber] = useState<number>();
+
+	useEffect(() => {
+		getRandomNumberFromApi().then((numb) => setNumber(numb));
+	}, []);
+
+	return (
+		<div className='App'>
+			<h2>Random Number: {number}</h2>
+		</div>
+	);
+};
+
+//https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new
